674a528ae3c43c17c0edbb62efe76315a084589e
IshanJawade <ishankjawade@gmail.com>
2020-09-14 14:52:19 +0530

    Deleted Elements are back with full funs

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -21,9 +21,32 @@ class Counter extends Component {
 
         return (
             <>     
+        {/* For counter printing */}
+            <span style={this.style} className={this.getDynamicBadges()}>{this.formatCount()}</span>
+
+        {/* For Button */}
+            <button className="btn btn-secondary btn-sm">Increament</button>
+
+        {/* to render unordered list */}
+            {/* in java script you can apply logical operations with non boolian things */}
+            {/* So this can be used as if statement without else part */}
+            { this.state.tags.length === 0 && 'Please create new tags' }
             {this.renderTags()}
             </>
         );
+    }
+
+    // for dynamic classes in "ClassName" created method getDynamicBadges()
+    getDynamicBadges() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    // to format text dynamically
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? "Zero": count;
     }
     
 }
